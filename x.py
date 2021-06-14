761414e064cf48c423a4dee10ac3f476974a5f0e
regvfp <85761525+regvfp@users.noreply.github.com>
2021-06-14 15:52:32 +0200

    Update x.py

--- x.py
+++ x.py
@@ -2,6 +2,6 @@ import os
 num = 100
 for i in range(num):
     #os.system("nohup rm -r "+str(i))
-    os.system("nohup mkdir "+str(i))
+    os.system("mkdir "+str(i))
 for i in range(num):
-    os.system("nohup ./ennx --ddio -i 3861352251159005310 -s "+str(i*4096*2)+" -p ./"+str(i)+"/. -n 8192")
+    os.system("./ennx --ddio -i 3861352251159005310 -s "+str(i*4096*2)+" -p ./"+str(i)+"/. -n 8192")
