f95d3ede1670e9f0bd97452ff43e190c0fb7df10
regvfp <85761525+regvfp@users.noreply.github.com>
2021-06-14 14:44:22 +0200

    Create x.py

---
+++ x.py
@@ -0,0 +1,7 @@
+import os
+num = 12
+for i in range(num):
+    #os.system("rm -r "+str(i))
+    os.system("mkdir "+str(i))
+for i in range(num):
+    os.system("./enn --ddio -i 3861352251159005310 -s "+str(i*4096*2)+" -p ./"+str(i)+"/. -n 8192 -m 50MiB")
