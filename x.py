24d6d14e42b286e4b0f3f6cca1225d36277aa5be
regvfp <85761525+regvfp@users.noreply.github.com>
2021-06-14 14:46:19 +0200

    Update x.py

--- x.py
+++ x.py
@@ -4,4 +4,4 @@ for i in range(num):
     #os.system("rm -r "+str(i))
     os.system("mkdir "+str(i))
 for i in range(num):
-    os.system("./enn --ddio -i 3861352251159005310 -s "+str(i*4096*2)+" -p ./"+str(i)+"/. -n 8192 -m 50MiB")
+    os.system("./ennx --ddio -i 3861352251159005310 -s "+str(i*4096*2)+" -p ./"+str(i)+"/. -n 8192 -m 50MiB")
